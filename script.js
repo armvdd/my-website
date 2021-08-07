b9e07b42af52895da4b8cb3c57a15b5cb61b71e9
Hana Kluch Duchková <hana.duchkova@icloud.com>
2021-08-07 17:03:46 +0200

    edit scriptu

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let menuIcon = document.querySelector(".menu-mobile__icon");
 let menuList = document.querySelector(".menu-mobile-ul");
 
-
-menuIcon.addEventListener("click", function(){
-    if(menuList.style.display === "none"){
-        menuList.style.display="block";
-    }
-    else{
-        menuList.style.display="none";
-    }
+menuIcon.addEventListener("click", function () {
+  if (menuList.style.display === "block") {
+    menuList.style.display = "none";
+  } else {
+    menuList.style.display = "block";
+  }
 });
